c8e273f5204b7d2d8182ad34715909c461dad41a
nk-aditya <adityanayak2803@gmail.com>
2026-04-28 03:38:52 +0530

    Created complaint model schema

---
+++ server/models/Complaint.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const complaintSchema = new mongoose.Schema(
+{
+  title: {
+    type: String,
+    required: true
+  },
+
+  description: {
+    type: String,
+    required: true
+  },
+
+  category: {
+    type: String,
+    enum: ["Electricity", "Water", "Internet", "Cleaning", "Furniture", "Other"],
+    default: "Other"
+  },
+
+  status: {
+    type: String,
+    enum: ["Pending", "Assigned", "In Progress", "Resolved"],
+    default: "Pending"
+  },
+
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null
+  }
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model("Complaint", complaintSchema);
